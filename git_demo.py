1cfa084967dcdbbd5052b0833e5b22c45197d73a
Vishwas R <Vishwasrvibhu@gmail.com>
2024-06-06 14:30:34 +0530

    ddt and xdist

--- git_demo.py
+++ git_demo.py
@@ -1,1 +1,22 @@
-# this is a sample file
+from selenium import webdriver
+from time import sleep
+
+
+def test_one():
+    driver = webdriver.Chrome()
+    driver.get("https:www.fb.com")
+    sleep(3)
+    driver.quit
+
+def test_two():
+    driver = webdriver.Chrome()
+    driver.get("https:www.instagram.com")
+    sleep(3)
+    driver.quit
+
+def test_three():
+    driver = webdriver.Chrome()
+    driver.get("https:www.yatra.com")
+    sleep(3)
+    driver.quit
+
